519da49d4d54349d9926c5b2e6dca0bdc2f8a366
hardware-scout <mukesh25202007@gamail.com>
2026-07-03 01:13:36 +0530

    Restore Google DNS for SRV resolution - needed for Atlas connection

--- db/connection.js
+++ db/connection.js
@@ -1,4 +1,8 @@
 import mongoose from 'mongoose';
+import dns from 'dns';
+
+// Use Google DNS for reliable SRV resolution on all networks
+dns.setServers(['8.8.8.8', '8.8.4.4']);
 
 const MONGO_URI = process.env.MONGO_URI || 'mongodb+srv://mktech:mktech3003@mktech1.k9ebxva.mongodb.net/mktech?retryWrites=true&w=majority';
 
